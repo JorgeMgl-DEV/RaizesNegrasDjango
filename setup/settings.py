23312a4445719b72d1cd8ce2617ef76405bc3727
JorgeMgl-DEV <jorgeditor.social@gmail.com>
2025-03-12 15:35:50 -0300

    carregamento dos arquivos estaticos

--- setup/settings.py
+++ setup/settings.py
@@ -130,3 +130,21 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# filepath: settings.py
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
